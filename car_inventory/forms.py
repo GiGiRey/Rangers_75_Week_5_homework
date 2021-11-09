dfff8ab209e7c295a61fe5426cebafc729cbb4c9
GiGiRey <86616436+GiGiRey@users.noreply.github.com>
2021-11-09 16:15:29 -0600

    replacement files

---
+++ car_inventory/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email
+
+class UserLoginForm(FlaskForm):
+    # email, password, submit_button
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    submit_button = SubmitField()
+
+class UserSignUpForm(FlaskForm):
+    # email, password, first_name, last_name, submit_button
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    first_name = StringField('First Name', validators = [DataRequired()])
+    last_name = StringField('Last Name', validators = [DataRequired()])
+    submit_button = SubmitField()
